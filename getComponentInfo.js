fe16cc0263647e6fe1f3ac3b355b0e868f08c58b
A3a3e1 <pavel.voropaiev@elastic.io>
2019-02-04 10:14:43 +0200

    added credentials verification

--- getComponentInfo.js
+++ getComponentInfo.js
@@ -24,10 +24,11 @@ async function readTeamCompIds() {
 async function getAllComponentIds() {
   const { data } = (await request.get(`/v2/teams/${TEAM_ID}`)).data;
   const contractId = data.relationships.contract.data.id;
-  const { data: allContractComponents } = (await request.get(`/v2/components?contract_id=${contractId}`)).data;
+  const { data: allContractComponents } = (await request.get(`/v2/components?contract_id=${contractId}&filter[access]=private`)).data;
   return Object.values(allContractComponents);
 }
 
+// Reads the list of components from the 'component-list.txt' file
 function readComponentsList() {
   const text = fs.readFileSync(`${process.argv[2]}/component-list.txt`).toString();
   const lines = text.split('\n').filter(l => !!l);
@@ -44,13 +45,10 @@ function readComponentsList() {
 
 async function findCommonComponents() {
   const componentsToPush = await readComponentsList();
-  // console.log(JSON.stringify(componentsToPush, null, 2));
   const allComps = await getAllComponentIds();
-  // console.log(JSON.stringify(allComps, null, 2));
   for (const allComp of allComps) {
     for (const componentToPush of componentsToPush) {
       if (allComp.attributes.name === componentToPush.component) {
-        // console.log(allComp.id);
         console.log(`${allComp.id}:${allComp.attributes.name}`);
       }
     }
